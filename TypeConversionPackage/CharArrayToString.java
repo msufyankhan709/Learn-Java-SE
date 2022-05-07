4925da81c3170d475231097b1e67d2e5ebaa1986
M.Sufyan Khan <67526983+msufyankhan709@users.noreply.github.com>
2022-05-07 17:11:49 +0100

    Type conversion changes

---
+++ TypeConversionPackage/CharArrayToString.java
@@ -0,0 +1,10 @@
+package TypeConversionPackage;
+
+public class CharArrayToString {
+    public static void main(String[] args) {
+        char[] ch= {'A','E','I','O','U'};
+        String name=String.valueOf(ch);
+        String name2=new String(ch);
+        System.out.println(name+" "+name2);
+    }
+}
---
+++ TypeConversionPackage/CharToString.java
@@ -0,0 +1,9 @@
+package TypeConversionPackage;
+
+public  class CharToString {
+    public static void main(String[] args) {
+        char ch='a';
+        String str= Character.toString(ch);
+        System.out.println(str);        
+    }
+}
