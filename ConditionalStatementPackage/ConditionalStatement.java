1aa90cf805f8b9aa94d8ee1dfe861c285e09c615
M.Sufyan Khan <67526983+msufyankhan709@users.noreply.github.com>
2022-05-10 14:30:55 +0100

    conditional statement

---
+++ ConditionalStatementPackage/ConditionalStatement.java
@@ -0,0 +1,14 @@
+package ConditionalStatementPackage;
+
+import java.util.Scanner;
+
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        int a=sc.nextInt();
+        int result=(x>a)?x:a;
+        System.out.println(result);
+    }
+}
